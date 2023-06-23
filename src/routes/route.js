03dd05c963acba4af33fed4e9591addf27b773ef
jhaprahlad <pkjha01234@gmail.com>
2023-06-23 11:35:51 +0530

    created authentication file

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,21 +1,22 @@
-const router= require("express").Router()
-const {createUser,loginUser}= require("../controllers/userController")
-const {createBook,getBookByQuery,getBookByParams,updateBook,deleteBook} = require("../controllers/bookController")
-const {createReview,updateReview,deletedReview}= require("../controllers/reviewController")
+const router = require("express").Router()
+const { createUser, loginUser } = require("../controllers/userController")
+const { createBook, getBookByQuery, getBookByParams, updateBook, deleteBook } = require("../controllers/bookController")
+const { createReview, updateReview, deletedReview } = require("../controllers/reviewController")
+const { authentication } = require("../middleware/authentication")
 
 
-router.post("/register",createUser)
-router.post("/login",loginUser)
+router.post("/register", createUser)
+router.post("/login", loginUser)
 
-router.post("/books",createBook)
-router.get("/books",getBookByQuery)
-router.get("/books/:bookId",getBookByParams)
-router.put("/books/:bookId",updateBook)
-router.delete("/books/:bookId",deleteBook)
+router.post("/books", authentication, createBook)
+router.get("/books", authentication, getBookByQuery)
+router.get("/books/:bookId", authentication, getBookByParams)
+router.put("/books/:bookId", authentication, updateBook)
+router.delete("/books/:bookId", authentication, deleteBook)
 
 router.post('/books/:bookId/review', createReview)
-router.put('/books/:bookId/review/:reviewId',updateReview)
-router.delete('/books/:bookId/review/:reviewId',deletedReview)
+router.put('/books/:bookId/review/:reviewId', updateReview)
+router.delete('/books/:bookId/review/:reviewId', deletedReview)
 
 
-module.exports=router
+module.exports = router
